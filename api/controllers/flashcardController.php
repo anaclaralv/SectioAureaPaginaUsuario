ee1a49b8e74c625e3b3d7c7ec1fea3d9857febdf
LaraNapoli08 <larafernapoli@gamil.com>
2026-08-10 16:52:23 -0300

    fix: corrigir flashcard

--- api/controllers/flashcardController.php
+++ api/controllers/flashcardController.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-class FlashController {
+class FlashcardController {
     private $flash;
     private $id_usuario_logado;
     
